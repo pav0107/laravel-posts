3f7ef359688d2cbb3df9e080d6e648162ed7b7b7
Pav Rao <pav0107@gmail.com>
2022-01-18 13:28:28 +0000

    basic styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,35 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Posty</title>
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a class="p-3" href="">Home</a>
+            </li>
+            <li>
+                <a class="p-3" href="">Dashboard</a>
+            </li>
+            <li>
+                <a class="p-3" href="">Post</a>
+            </li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li>
+                <a class="p-3" href="">Pav Rao</a>
+            </li>
+            <li>
+                <a class="p-3" href="">Login</a>
+            </li>
+            <li>
+                <a class="p-3" href="">Register</a>
+            </li>
+            <li>
+                <a class="p-3" href="">Logout</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
