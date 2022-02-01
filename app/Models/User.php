611871b2f9d4194342eb7a6404159ffe92a3b9e3
Pav Rao <pav0107@gmail.com>
2022-02-01 15:30:48 +0000

    liking and unliking posts

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Like;
+use App\Models\Post;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -46,5 +48,10 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,6 +30,26 @@
 
                         <p class="mb-2">{{ $post->body }}</p>
                     </div>
+
+                    <div class="flex items-center">
+
+                        @if (!$post->likedBy(auth()->user()))
+                            <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+                                @csrf
+                                <button type="submit" class="text-blue-500">Like</button>
+                            </form>
+                        @else
+                            <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                @csrf 
+                                @method('DELETE')                
+                                <button type="submit" class="text-blue-500">Unlike</button>
+                            </form>
+                        @endif
+
+                        <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}
+                    </div>
+
+
                 @endforeach
                 
                 {{ $posts->links() }}
