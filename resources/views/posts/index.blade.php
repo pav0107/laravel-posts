ba17ef6e579e407daa27db1a0730507d71c7d823
Pav Rao <pav0107@gmail.com>
2022-02-01 13:58:39 +0000

    seeding with model factories

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -31,7 +31,9 @@
                         <p class="mb-2">{{ $post->body }}</p>
                     </div>
                 @endforeach
+                
                 {{ $posts->links() }}
+                
             @else
                 <p>There are no posts</p>
             @endif
