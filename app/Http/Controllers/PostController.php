7e8c3c0320165b39861ef7b3d926e12cad143d47
Pav Rao <pav0107@gmail.com>
2022-02-01 12:02:40 +0000

    pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,8 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::get();
-        
+        $posts = Post::paginate(2);
+
         return view('posts.index', [
             'posts' => $posts
         ]);
