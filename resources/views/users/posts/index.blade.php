d16960db5ae9196327cbf596a65571f001bc65fa
Pav Rao <pav0107@gmail.com>
2022-02-04 13:49:08 +0000

    Like count Eloquent relationship

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -5,7 +5,7 @@
         <div class="w-8/12">
             <div class="p-6">
                 <h1 class="mb-1 text-2xl font-medium">{{ $user->name }}</h1>
-                <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }}</p>
+                <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }} and received {{ $user->receivedLikes->count() }} likes</p>
             </div>
 
             <div class="p-6 bg-white rounded-lg">
