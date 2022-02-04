34f35e2ae33cb9eae27b04ff55f6e441013f69c1
Pav Rao <pav0107@gmail.com>
2022-02-04 15:00:24 +0000

    some tidying up

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,24 +3,25 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 p-6 bg-white rounded-lg">
-            <form action="{{  route('posts') }}" method="post" class="mb-4">
-                @csrf
-                <div class="mb-4">
-                    <label for="body" class="sr-only">Body</label>
-                    <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post something!"></textarea>
+            @auth
+                <form action="{{  route('posts') }}" method="post" class="mb-4">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="body" class="sr-only">Body</label>
+                        <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post something!"></textarea>
 
-                    @error('body')
-                        <div class="mt-2 text-sm text-red-500">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
+                        @error('body')
+                            <div class="mt-2 text-sm text-red-500">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
 
-                <div>
-                    <button type="submit" class="px-4 py-2 font-medium text-white bg-blue-500 rounded">Post</button>
-                </div>                
-            
-            </form>
+                    <div>
+                        <button type="submit" class="px-4 py-2 font-medium text-white bg-blue-500 rounded">Post</button>
+                    </div>                     
+                </form>
+            @endauth
 
             @if ($posts->count())
                 @foreach ($posts as $post)
