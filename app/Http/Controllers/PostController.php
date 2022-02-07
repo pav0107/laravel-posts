5da0979299864b22efbdea0b491d5b8803f19a07
Pav Rao <pav0107@gmail.com>
2022-02-07 11:03:25 +0000

    updated pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
 
     public function index()
     {
-        $posts = Post::latest()->with(['user', 'likes'])->paginate(3);
+        $posts = Post::latest()->with(['user', 'likes'])->paginate(20);
 
         return view('posts.index', [
             'posts' => $posts
